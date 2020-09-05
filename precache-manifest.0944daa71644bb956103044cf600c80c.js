731369b7425f89e18193aaf6844d97eed4fa55e6
irinazhuravleva <irinazhuravleva@yandex.ru>
2020-09-05 21:32:19 +0300

    first commit

---
+++ precache-manifest.0944daa71644bb956103044cf600c80c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d12f2d96a7b82623a92dc4a3b69a2c0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7115a4b5275721a2f8e",
+    "url": "/static/css/2.06c478a6.chunk.css"
+  },
+  {
+    "revision": "d91aeddce1957b4ea4a8",
+    "url": "/static/css/main.8e3f9a42.chunk.css"
+  },
+  {
+    "revision": "b7115a4b5275721a2f8e",
+    "url": "/static/js/2.1dc7f5b2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.1dc7f5b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d91aeddce1957b4ea4a8",
+    "url": "/static/js/main.61b35154.chunk.js"
+  },
+  {
+    "revision": "89ac118f8d664424e2aa",
+    "url": "/static/js/runtime-main.40fd9fda.js"
+  },
+  {
+    "revision": "4c52b3f83973bd5c7626fa538805bf99",
+    "url": "/static/media/ocean.4c52b3f8.jpeg"
+  }
+]);
